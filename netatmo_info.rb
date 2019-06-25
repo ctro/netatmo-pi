5dd9d287fc0a56db03433d82e9bbe0ac18b1a13b
ctro <clint@ctro.net>
2019-06-25 12:39:18 -0600

    Remove wird characters

--- netatmo_info.rb
+++ netatmo_info.rb
@@ -13,7 +13,7 @@ class NetatmoInfo
   def inside_string
     id = @data["body"]["devices"].first["dashboard_data"]
 
-    "In :#{c_to_f(id["Temperature"])}°#{arrow(id["temp_trend"])} " \
+    "In :#{c_to_f(id["Temperature"])}° #{arrow(id["temp_trend"])} " \
     "#{id["Humidity"]}% " \
     "#{id["AbsolutePressure"]}mm #{arrow(id["pressure_trend"])} " \
     "#{id["CO2"]}ppm #{id["Noise"]}db"
@@ -67,13 +67,13 @@ class NetatmoInfo
   def arrow(direction)
     case direction
     when "stable"
-      "️️="
+      "="
     when "up"
       "+"
     when "down"
-      "️-"
+      "-"
     else
-      "‍WUT"
+      "WUT"
     end
   end
 
