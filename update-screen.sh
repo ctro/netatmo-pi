482eacc305a715d45d8b27b537501ef1b7fad46d
ctro <clint@ctro.net>
2019-07-19 21:51:30 -0600

    file rename

--- update-screen.sh
+++ update-screen.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # netatmo-api hits the api and writes a .json file
-cd netatmo-api
-ruby get_api_response.rb
+cd netatmo-api && ruby write_api_response.rb
 
 # papirus-display reads the .json file and displays it nicely
 cd ../papirus-display
